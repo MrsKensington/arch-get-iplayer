e8f4c73564c7f3863d5fbf825d91f61aa012a901
Michael <mike@mikeditum.co.uk>
2017-05-05 10:37:09 +0100

    Accidentally reverted the change when I merged

--- setup/nobody/start.sh
+++ setup/nobody/start.sh
@@ -41,7 +41,7 @@ do
 
 		echo "[info] Running get_iplayer..."
 		# run get_iplayer for show, saving to incomplete folder
-		/usr/bin/get_iplayer --profile-dir /config --get --nopurge --modes=flashhd,flashvhigh,flashhigh,flashstd,flashnormal,flashlow --file-prefix="${show_name} - <senum> - <episodeshort>" "${show_name}" --output "/data/get_iplayer/incomplete/${show_name}"
+		/usr/bin/get_iplayer --type=${TYPES} --profile-dir /config --get --nopurge --modes="${MODES}" --file-prefix="${show_name} - <senum> - <episodeshort>" "${show_name}" --output "/data/get_iplayer/incomplete/${show_name}"
 
 	done
 
